998127aaf49c069e305d304173b2b50c4cc254e0
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-10-15 21:28:42 +0300

    Добавляет страницы в приложение

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import MainScreen from '../../pages/welcome-screen/main-screen';
+import MainScreen from '../../pages/main-screen/main-screen';
 
 type AppScreenProps = {
   offerCardsCount: number;
