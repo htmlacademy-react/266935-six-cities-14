0c30373a2a85ff09f642994eaea5d43025e83410
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-10-18 14:48:11 +0300

    Обновляет ссылку для логотипа на страницах

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -1,3 +1,4 @@
+import Logo from '../../components/logo/logo';
 import OfferCard from '../../components/offer-card/offer-card';
 
 type MainScreenProps= {
@@ -10,11 +11,7 @@ function MainScreen({offerCardsCount}: MainScreenProps): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
+            <Logo/>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
