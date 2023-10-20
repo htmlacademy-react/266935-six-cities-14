22c2daa55f3a1ab0069b2ae01ff586bb90d0b16e
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-10-20 13:49:43 +0300

    Добавляет константу в логотип

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,9 +1,10 @@
+import { AppRoute } from '../../const';
 import {Link} from 'react-router-dom';
 
 function Logo(): JSX.Element {
   return (
     <div className="header__left">
-      <Link className="header__logo-link" to="/">
+      <Link className="header__logo-link" to={AppRoute.Root}>
         <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
       </Link>
     </div>
