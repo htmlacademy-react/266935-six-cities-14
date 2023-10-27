2646a225c78359c3eeb17b97d86092f75292cca2
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-10-27 11:10:06 +0300

    Добавляет двустороннее связывание для PostReviewForm

--- src/components/post-review-form/post-review-form.tsx
+++ src/components/post-review-form/post-review-form.tsx
@@ -9,6 +9,17 @@ function PostReviewForm({onCommentPost}: PostReviewFormProps): JSX.Element {
 
   const [selectedRating, setSelectedRating] = useState(0);
   const [textReview, setTextReview] = useState('');
+  const isValid = textReview.length >= 50 && textReview.length <= 150 && selectedRating !== 0;
+
+  const handleTextAreaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setTextReview(event.target.value);
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value !== undefined){
+      setSelectedRating(parseInt(event.target.value, 10));
+    }
+  };
 
   return (
     <form
@@ -22,10 +33,14 @@ function PostReviewForm({onCommentPost}: PostReviewFormProps): JSX.Element {
       <div className="reviews__rating-form form__rating">
         {[5,4,3,2,1].map((number) => (
           <Fragment key={number}>
-            <input className="form__rating-input visually-hidden" name="rating" value={number} id={`${number}-stars`} type="radio"
-              onChange={() => {
-                setSelectedRating(number);
-              }}
+            <input
+              className="form__rating-input visually-hidden"
+              name="rating"
+              value={number}
+              id={`${number}-stars`}
+              type="radio"
+              checked={number === selectedRating}
+              onChange={handleInputChange}
             />
             <label htmlFor={`${number}-stars`} className="reviews__rating-label form__rating-label" title="perfect">
               <svg className="form__star-image" width="37" height="33">
@@ -40,9 +55,8 @@ function PostReviewForm({onCommentPost}: PostReviewFormProps): JSX.Element {
         className="reviews__textarea form__textarea"
         id="review" name="review"
         placeholder="Tell how was your stay, what you like and what can be improved"
-        onChange={({target}: ChangeEvent<HTMLTextAreaElement>) => {
-          setTextReview(target.value);
-        }}
+        value={textReview}
+        onChange={handleTextAreaChange}
       >
 
       </textarea>
@@ -50,7 +64,7 @@ function PostReviewForm({onCommentPost}: PostReviewFormProps): JSX.Element {
         <p className="reviews__help">
       To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit">Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={!isValid}>Submit</button>
       </div>
     </form>
   );
