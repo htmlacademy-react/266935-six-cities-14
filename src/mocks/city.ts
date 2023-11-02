a65861b340dbc26562268a30fee173a829bb3b94
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-11-02 10:29:32 +0300

    Добавляет моковые данные для отображения карты

---
+++ src/mocks/city.ts
@@ -0,0 +1,12 @@
+import { City } from '../types/offer';
+
+export const Cities: City [] = [
+  {
+    name: 'Amsterdam',
+    location: {
+      latitude: 52.37454,
+      longitude: 4.897976,
+      zoom: 13
+    }
+  }
+];
