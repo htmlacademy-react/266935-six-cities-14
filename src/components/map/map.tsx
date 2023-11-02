86a340307e1482152f42ec9d133c1cd853dfdb19
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-11-02 11:01:33 +0300

    Добавляет кастомные маркеры для карты

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
-import leaflet from 'leaflet';
+import {Icon, Marker, layerGroup} from 'leaflet';
+import { URL_MARKER_DEFAULT, URL_MARKER_CURRENT } from '../../const';
 import 'leaflet/dist/leaflet.css';
 
 import { City } from '../../types/offer';
@@ -9,6 +10,18 @@ import useMap from '../../hooks/use-map';
 type MapProps = {
     city: City;
 };
+
+const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
 
 function Map({city} : MapProps): JSX.Element {
   const mapRef = useRef(null);
