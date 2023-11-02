deb01857ae60786f19f8b97772a5a151bd1226db
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-11-02 10:37:39 +0300

    Создает объект с использованием useRef и связывает его с контейнером для карты

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import { useRef } from 'react';
 import leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 function Map() {
+  const mapRef = useRef(null);
+
   return (
     <section
       className="cities__map map"
       style={{height: '500px'}}
+      ref={mapRef}
     >
 
     </section>
