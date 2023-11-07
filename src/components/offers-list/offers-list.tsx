af98dd8f7af2a1bb2146d2cec99654c2ebcad44f
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-11-07 16:52:40 +0300

    Добавляет компонент SortType

--- src/components/offers-list/offers-list.tsx
+++ src/components/offers-list/offers-list.tsx
@@ -3,6 +3,7 @@ import { Cities } from '../../mocks/city';
 import Map from '../map/map';
 
 import OfferCard from '../offer-card/offer-card';
+import SortType from '../sort-type/sort-type';
 
 import {useState, Fragment} from 'react';
 
@@ -30,21 +31,7 @@ function OffersList({offerCardType, offers, selectedCity}: OffersListProps): JSX
       <section className="cities__places places">
         <h2 className="visually-hidden">Places</h2>
         <b className="places__found">{offersInSelectedCity.length} places to stay in {selectedCity}</b>
-        <form className="places__sorting" action="#" method="get">
-          <span className="places__sorting-caption">Sort by</span>
-          <span className="places__sorting-type" tabIndex={0}>
-          Popular
-            <svg className="places__sorting-arrow" width="7" height="4">
-              <use xlinkHref="#icon-arrow-select"></use>
-            </svg>
-          </span>
-          <ul className="places__options places__options--custom places__options--opened">
-            <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-            <li className="places__option" tabIndex={0}>Price: low to high</li>
-            <li className="places__option" tabIndex={0}>Price: high to low</li>
-            <li className="places__option" tabIndex={0}>Top rated first</li>
-          </ul>
-        </form>
+        <SortType />
         <div className="cities__places-list places__list tabs__content">
           {offersInSelectedCity.map((offer) => (
             <OfferCard
