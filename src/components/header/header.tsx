3c98f0844e4083f81256b3a0543910bea8799402
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-11-07 16:21:20 +0300

    Исправляет Link в header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,14 +12,12 @@ function Header(): JSX.Element {
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="#">
+                <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
                   <div className="header__avatar-wrapper user__avatar-wrapper">
                   </div>
-                  <Link className="header__logo-link" to={AppRoute.Favorites}>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </Link>
-                </a>
+                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                  <span className="header__favorite-count">3</span>
+                </Link>
               </li>
               <li className="header__nav-item">
                 <a className="header__nav-link" href="#">
