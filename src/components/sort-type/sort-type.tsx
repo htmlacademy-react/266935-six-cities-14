62a43e1ba15e2b3a03bc31cd6c95501fe0b9e15b
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-11-09 15:59:15 +0300

    Добавляет скрытие списка сортировки при клике на свободное место

--- src/components/sort-type/sort-type.tsx
+++ src/components/sort-type/sort-type.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { SortTypesSetting } from '../../const';
 import { SortTypes } from '../../types/sort-types';
 
@@ -10,22 +10,41 @@ type SortTypeProps= {
 function SortType({sortTypeSetting, setSortType}: SortTypeProps): JSX.Element {
 
   const [sortListVisible, setSortListVisible] = useState<boolean>(false);
+  const listRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if(!sortListVisible) {
+      return;
+    }
+    const openArea = document.querySelector('.places__sorting-type');
+    const handleClick = (evt: MouseEvent) => {
+      if(!listRef.current?.contains(evt.target as Node) && evt.target !== openArea){
+        setSortListVisible(false);
+      }
+    };
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, [sortListVisible]);
 
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={() => setSortListVisible(!sortListVisible)}
+      >
         {SortTypesSetting[sortTypeSetting]}
-        <svg
-          className="places__sorting-arrow"
-          width="7"
-          height="4"
-          onClick={() => setSortListVisible(!sortListVisible)}
-        >
+        <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
       <ul
+        ref={listRef}
         className={`${sortListVisible ?
           'places__options places__options--custom places__options--opened' :
           'places__options places__options--custom'}`}
