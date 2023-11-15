f7cd4c63d9077893cf59bd64687362ad247a8eb9
kirill-cite <kirill.kysh06041996@yandex.ru>
2023-11-15 17:02:29 +0300

    Добавляет компонент для отображения ошибки и подключает его

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 
 import App from './components/app/app';
+import ErrorMessage from './components/error-message/error-message';
 
 import { reviews } from './mocks/reviews';
 import {store} from './store';
@@ -14,6 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
+      <ErrorMessage />
       <App
         reviews = {reviews}
       />
